100d0431d69e850d40f2593b89e88116325d1306
lucky <ajaykhati532@gmail.com>
2024-11-28 06:55:23 +0530

    first

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getRandomNumber(min, max) {
   if (min > max) {
     throw new Error("Min value should be less than or equal to Max value");
-  }
+  }     
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
